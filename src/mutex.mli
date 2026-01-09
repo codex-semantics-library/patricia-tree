172a39fdda74f0af7dbb88a9ce7149532420459b
Dorian Lesbre <dorian.lesbre@gmail.com>
2026-01-09 17:33:40 +0100

    Add Mutex protect for all variants

--- src/mutex.mli
+++ src/mutex.mli
@@ -11,13 +11,31 @@ open Signatures
     {!MUTEX.unlock} call, allowing to use these unsafe constructs in a
     multi-threaded context. *)
 
+(** Add {!MUTEX} protection around a {!MAP}.
+
+    @canonical PatriciaTree.MutexProtectMap
+    @since 0.12.0 *)
+module MutexProtectMap(Map: MAP)(Mutex: MUTEX) : MAP with type key = Map.key
+
+(** Add {!MUTEX} protection around a {!SET}.
+
+    @canonical PatriciaTree.MutexProtectSet
+    @since 0.12.0 *)
+module MutexProtectSet(Set: SET)(Mutex: MUTEX) : SET with type elt = Set.elt
+
 (** Add {!MUTEX} protection around a {!HETEROGENEOUS_MAP}.
 
     @canonical PatriciaTree.MutexProtectHeterogeneousMap
     @since 0.12.0 *)
-module MutexProtectHeterogeneousMap
-  (Map: HETEROGENEOUS_MAP)
-  (Mutex: MUTEX)
+module MutexProtectHeterogeneousMap(Map: HETEROGENEOUS_MAP)(Mutex: MUTEX)
 : HETEROGENEOUS_MAP
     with type 'k key = 'k Map.key
      and type ('k, 'm) value = ('k, 'm) Map.value
+
+(** Add {!MUTEX} protection around a {!HETEROGENEOUS_SET}.
+
+    @canonical PatriciaTree.MutexProtectHeterogeneousSet
+    @since 0.12.0 *)
+module MutexProtectHeterogeneousSet(Set: HETEROGENEOUS_SET)(Mutex: MUTEX)
+: HETEROGENEOUS_SET
+  with type 'a elt = 'a Set.elt
